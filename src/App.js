60e42310af031f63364984f55f838ccdee26a4a5
Kevin Baktiar <afterafx@gmail.com>
2019-08-21 09:27:16 -0700

    continuing homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import './css/main.scss';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { Switch, Route} from 'react-router-dom';
 import { NavBar, HomePage, PathInfo, CatalogPage, SupportPage, DashboardPage  } from './components';
 // import { LessonInfo } from './components';
 
