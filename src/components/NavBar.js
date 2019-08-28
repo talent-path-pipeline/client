dce826b289a7d11cf9502f7ff546ca351c44cd8b
Kevin Baktiar <afterafx@gmail.com>
2019-08-28 09:31:18 -0700

    fixed a few css bugs

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { slide as Menu } from 'react-burger-menu';
 import '../css/NavBar.scss';
+import '../css/BurgerMenu.scss';
 
 const links = {
   home: '/',
@@ -11,15 +13,63 @@ const links = {
   dashboard: '/dashboard',
 };
 
-const NavBar = () => <div id="nav-main">
-  <NavLink to={links.home}><img id="nav-image" src="./images/logo.png" alt="main-logo"/></NavLink>
-  <ul id="nav-list">
-    <li><NavLink to={links.paths}>Paths</NavLink></li>
-    <li><NavLink to={links.catalog}>Catalog</NavLink></li>
-    <li><NavLink to={links.support}>Support</NavLink></li>
-    <li><NavLink to={links.about}>About</NavLink></li>
-    <li><NavLink to={links.dashboard}>Dashboard</NavLink></li>
-  </ul>
-</div>;
+class NavBar extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state ={
+      menuOpen: false,
+    };
+  }
+
+  // This keeps your state in sync with the opening/closing of the menu
+  // via the default means, e.g. clicking the X, pressing the ESC key etc.
+  handleStateChange(state) {
+    this.setState({ menuOpen: state.isOpen });
+  }
+
+  // This can be used to close the menu, e.g. when a user clicks a menu item
+  closeMenu() {
+    this.setState({ menuOpen: false });
+  }
+
+  // This can be used to toggle the menu, e.g. when using a custom icon
+  // Tip: You probably want to hide either/both default icons if using a custom icon
+  // See https://github.com/negomi/react-burger-menu#custom-icons
+  toggleMenu() {
+    const { menuOpen } = this.state;
+    this.setState({ menuOpen: !menuOpen });
+  }
+
+  render() {
+    const { menuOpen } = this.state;
+
+    return (
+      <div id="nav-main">
+        <NavLink to={links.home}><h1 id="nav-title">Stonehaven Academy</h1></NavLink>
+        <ul className="desktop-nav">
+          <li><NavLink to={links.paths}>Paths</NavLink></li>
+          <li><NavLink to={links.catalog}>Catalog</NavLink></li>
+          <li><NavLink to={links.support}>Support</NavLink></li>
+          <li><NavLink to={links.about}>About</NavLink></li>
+          <li><NavLink to={links.dashboard}>Dashboard</NavLink></li>
+        </ul>
+        <div className="mobile-nav">
+          <Menu
+            right
+            isOpen={menuOpen}
+            onStateChange={state => this.handleStateChange(state)}
+          >
+            <NavLink onClick={() => this.closeMenu()} to={links.home}>Home</NavLink>
+            <NavLink onClick={() => this.closeMenu()} to={links.paths}>Paths</NavLink>
+            <NavLink onClick={() => this.closeMenu()} to={links.catalog}>Catalog</NavLink>
+            <NavLink onClick={() => this.closeMenu()} to={links.about}>About</NavLink>
+            <NavLink onClick={() => this.closeMenu()} to={links.dashboard}>Dashboard</NavLink>
+          </Menu>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default NavBar;
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../../css/pages/HomePage.scss'
-import Glide from '@glidejs/glide'
+import Glide from '@glidejs/glide/'
 
 class HomePage extends React.Component {
   constructor(props) {
@@ -12,99 +12,68 @@ class HomePage extends React.Component {
   }
 
   componentDidMount = () => {
-    new Glide('.glide').mount()
+    new Glide('.glide').mount();
   }
 
   render () {
     return (
       <div>
-        {/* <img id="main-splash" src="./images/sample-splash.jpg" alt="main-splash"/> */}
-        <video id="main-splash" autoPlay loop="true">
-          <source src="./videos/tp-homepage-video.mp4" type="video/mp4"/>
-          {/* <source src="movie.ogg" type="video/ogg"/> */}
-          <h1>Welcome to Talent Path Fleet</h1>
-        </video>
+        <section id="splash-container">
+          <div id="call-to-action">
+            <h3>Start your path as a Dungeon Master Today</h3>
+            <button type="button">Click here</button>
+          </div>
+          <img className="splash-image" src="./images/splash-main2.png" alt=""/>
+          {/* <svg width="100%" height="20px">
+            <clipPath id="clipPolygon">
+              <polygon points="-1 419,960 421,0 367" />
+            </clipPath>
+          </svg> */}
+        </section>
+        <div id="company-name">
+          <h2>Welcome to</h2>
+          <h2>Stonehaven Academy</h2>
+          {/* <img id="path-image" src="./images/home-path-image.jpg" alt="Web Development Image" /> */}
+        </div>
         <div id="company-information">
           <div id="company-grid">
             <section className="company-box">
-              <img src="https://img.icons8.com/ios-filled/50/000000/e-learning.png"/>
+              <img src="https://img.icons8.com/windows/64/000000/dungeons-and-dragons--v2.png"/>
               <div className="company-info">
-                <p>Talent Path is unlike anything you’ve seen. Bootcamps, academies, and online learning platforms all have two things in common: you pay them to learn and there is no guarantee of a job once completed.</p>
+                <p>Here at Stonehaven Academy, we focused strictly on teaching you how to play Dungeons and Dragons. This game requires an immense amount
+                  of information that we are willing to teach you!
+                </p>
               </div>
             </section>
             <section className="company-box">
-              <img src="https://img.icons8.com/ios-filled/50/000000/communication-skill.png" />
+              <img src="https://img.icons8.com/ios-filled/64/000000/magical-scroll.png" />
               <div className="company-info">
-                <p>We’re unique because we hire you then pay you to learn hugely valuable skills and technologies for which there is immediate demand in the marketplace. Then we put you in a career launching role with a leading employer. </p>
+                <p>We’re unique because we have curated paths for you to follow as well as different courses so that you can gain all the information you need to start playing. </p>
               </div>
             </section>
             <section className="company-box">
-              <img src="https://img.icons8.com/ios-filled/50/000000/investment.png" />
+              <img src="https://img.icons8.com/ios-filled/64/000000/icosahedron.png"/>
               <div className="company-info">
-                <p>Get paid to learn and launch your career? We know it might sound too good to be true. We can afford to make an upfront investment in your career because clients help us recoup our investment during your tenure as a Talent Path consultant</p>
-              </div>
-            </section>
-            <section className="company-box">
-              <img src="https://img.icons8.com/ios-filled/50/000000/knowledge-sharing.png" />
-              <div className="company-info">
-                <p>You get the knowledge, skills, and first role you need to launch your career. And our work with clients allows us to keep doing what we do for future generations of Talent Path consultants. It’s that simple. No catch.</p>
+                <p>Our goal is to help you understand those rolls and how to setup the perfect game. Join us in this journey to become the ultimate D&D master!</p>
               </div>
             </section>
           </div>
         </div>
-        <hr/>
-        <div id="start-path">
-          <h2>Start your Path today!</h2>
-          <img id="path-image" src="./images/home-path-image.jpg" alt="Web Development Image" />
-          <h2 id="path-text">Web Development</h2>
-        </div>
-        <hr/>
         <div id="testimonials">
-          <h2>Testimonials</h2>
           <div className="glide">
             <div className="glide__track" data-glide-el="track">
               <ul className="glide__slides">
                 <li className="glide__slide">
-                  <p className="testimonial-text">
-                  “I feel like I'm learning a lot and this is a really unique opportunity.
-                   We're treated really well and all the management and all the other people in the program are great,
-                    and I'm really enjoying myself. It barely feels like a real job because of how much I'm enjoying coming to work every day.”
-                  </p>
-                  <div className="testimonial-author-container">
-                    <h4>Sam Saks-Fithian</h4>
-                    <h6>Talent Path Consultant</h6>
-                  </div>
+                  <p>This is where a testimonal goes about how great it was to learn how to become a dungoen master</p>
                 </li>
                 <li className="glide__slide">
-                  <p className="testimonial-text">
-                  "You could see the value of the program. And we were impressed.
-                   I report to the Interim CTO of our Americas technology group and right away – within a week or so
-                    – I had no reservations about putting our Talent Path consultants in front of him to present some
-                     of what they worked on when they first arrived."
-                  </p>
-                  <div className="testimonial-author-container">
-                    <h4>Director -Strategy & Operations</h4>
-                    <h6>Fortune 100 Food Services Company</h6>
-                  </div>
-                </li>
-                <li className="glide__slide">
-                  <p className="testimonial-text">
-                  “I really feel comfortable that I am learning more and refining my skills before the workplace.
-                   I also feel like this is allowing me to rediscover my comfort zone in public
-                    - because I had really become introverted since High School,
-                     but I am finally coming out of my shell and being active again in conversations and participation
-                      - and it feels good honestly.”
-                  </p>
-                  <div className="testimonial-author-container">
-                    <h4>Brock Jameson</h4>
-                    <h6>Talent Path Consultant</h6>
-                  </div>
+                  <p>This is where a testimonal goes about how great it was to learn how to become a dungoen master</p>
                 </li>
               </ul>
             </div>
-            <div className="glide__arrows" data-glide-el="controls">
-              <button type="button" className="glide__arrow glide__arrow--left" data-glide-dir="<">Back</button>
-              <button type="button" className="glide__arrow glide__arrow--right" data-glide-dir=">">Next</button>
+            <div className="glide__bullets" data-glide-el="controls[nav]">
+              <button type="button" className="glide__bullet" data-glide-dir="=0" />
+              <button type="button" className="glide__bullet" data-glide-dir="=1" />
             </div>
           </div>
         </div>
