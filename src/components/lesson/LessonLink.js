6d16541a2e3dcdf5a38e6528ad4bd8c9e03b2696
emily-yang <cigoua@gmail.com>
2019-08-29 21:11:15 -0700

    format video durations

--- src/components/lesson/LessonLink.js
+++ src/components/lesson/LessonLink.js
@@ -1,21 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const formatTime = seconds => {
+  const h = Math.floor(seconds / 3600);
+  const m = Math.floor(seconds % 3600 / 60);
+  const s = Math.floor(seconds % 3600 % 60);
+
+  const hStr = h > 0 ? `${h}:` : '';
+  const mStr = `${m < 10 ? '0':'' }${m}:`
+  const sStr = `${s < 10 ? '0':'' }${s}`
+
+  return `${hStr}${mStr}${sStr}`;
+}
+
 const LessonLink = ({ title, src, length, active }) => (
   <div className='lesson-link'>
     {
       active
-        ?        (<div className="active-video">
-          {title}
-          <p>{length}</p>
-        </div>)
-        :        (<div className="link">
-          <a href={src}>{title}</a>
-          <p>{length}</p>
-        </div>)
+        ?  (
+          <div className="active-video">
+            {title}
+            <p>{formatTime(length)}</p>
+          </div>
+        )
+        : (
+          <div className="link">
+            <a href={src}>{title}</a>
+            <p>{formatTime(length)}</p>
+          </div>
+        )
     }
   </div>
 );
+
 
 LessonLink.propTypes = {
   title: PropTypes.string.isRequired,
