e1b3cf977e29b31d1785c03a259fe712862a1236
Sam Saks-Fithian <samsaksfithian@gmail.com>
2019-09-16 08:51:05 -0700

    videos are now all different for navigation testing purposes

--- src/DUMMY_DATA.js
+++ src/DUMMY_DATA.js
@@ -8,7 +8,7 @@ const example_session = {
 
 const sourcing_books = {
   order: 1,
-  src: 'https://www.youtube.com/embed/MFCzwiqlnCE',
+  src: 'https://www.youtube.com/embed/bL43rhEsU_g',
   length: 3344,
   title: 'Sourcing Your Books',
   description:
@@ -17,7 +17,7 @@ const sourcing_books = {
 
 const sourcing_equipment = {
   order: 2,
-  src: 'https://www.youtube.com/embed/MFCzwiqlnCE',
+  src: 'https://www.youtube.com/embed/AlQWsEYdQ-I',
   length: 3344,
   title: 'Sourcing Your Equipment',
   description:
@@ -26,7 +26,7 @@ const sourcing_equipment = {
 
 const choices = {
   order: 3,
-  src: 'https://www.youtube.com/embed/MFCzwiqlnCE',
+  src: 'https://www.youtube.com/embed/VKx1QG86wbo',
   length: 4475,
   title: 'Choices, Choices...',
   description: `If you've decided that DMing is for you, you'll have to decide what kind of campaign to run before you get started. Homebrew or storebought? Battle grid or Theater of the Mind? Character-focused or combat-heavy? There's more than one way to roll the dice, and this video will walk you through the different options so you know what's best for you.`,
@@ -34,7 +34,7 @@ const choices = {
 
 const finding_a_party = {
   order: 4,
-  src: 'https://www.youtube.com/embed/MFCzwiqlnCE',
+  src: 'https://www.youtube.com/embed/z7ArhJ-5tOY',
   length: 1343,
   title: 'Finding Your Party',
   description: `So you know what story you'd like to tell... now it's time to find the characters! This lesson walks through different places you can find potential players, and how to choose who will join your table.`,
@@ -64,7 +64,7 @@ const adventure_begins = {
 
 const make_rules = {
   order: 0,
-  src: 'https://www.youtube.com/embed/MFCzwiqlnCE',
+  src: 'https://www.youtube.com/embed/aBOH8YLUPjE',
   length: 3603,
   title: 'You Make the Rules!',
   description: `The published D&D sourcebooks have their fair share of suggested rules... but as the DM, you have the special privilege to bend those to your will! Learn in this video how to set house rules for your campaign and your world, and begin collecting these guidelines in a Player's Guide for your party.`,
@@ -72,7 +72,7 @@ const make_rules = {
 
 const expectations = {
   order: 1,
-  src: 'https://www.youtube.com/embed/MFCzwiqlnCE',
+  src: 'https://www.youtube.com/embed/j492O6w9BCw',
   length: 3603,
   title: 'Setting Expectations',
   description: `If your players are expecting a lighthearted comedy romp through the meadows, but you planned a dark, sober story of political intrigue... Nobody is going to have a good time. This video talks about aligning with your players on what your campaign will be... and coming to a compromise when you don't already agree.`,
@@ -80,7 +80,7 @@ const expectations = {
 
 const etiquette = {
   order: 2,
-  src: 'https://www.youtube.com/embed/MFCzwiqlnCE',
+  src: 'https://www.youtube.com/embed/fWZDuFIYkf0',
   length: 3603,
   title: 'You Make the Rules!',
   description: `D&D is all about having fun, and the best way to make sure that happens is to treat everyone around the table with respect. In this lesson, learn best practices to maintain harmony with your party members--both in and outside the game!`,
@@ -88,7 +88,7 @@ const etiquette = {
 
 const char_creation = {
   order: 3,
-  src: 'https://www.youtube.com/embed/MFCzwiqlnCE',
+  src: 'https://www.youtube.com/embed/EGeQvOElwmY',
   length: 3603,
   title: 'Character Creation',
   description: `At this point, your players should be building their characters--and you should be helping! All the data on those character sheets can be daunting, but with this video, you can get comfortable with what it all means.`,
@@ -111,7 +111,7 @@ const countdown_session0 = {
 
 const flavortown = {
   order: 0,
-  src: 'https://www.youtube.com/embed/MFCzwiqlnCE',
+  src: 'https://www.youtube.com/embed/AZkn4geEMck',
   length: 3603,
   title: 'Flavortown',
   description: `Settings are better when they're spruced up with smells, sights, and sounds! Learn how to describe environments and situations that your characters find themselves in with aplomb to fully immerse your players in the fantasy.`,
@@ -119,7 +119,7 @@ const flavortown = {
 
 const actor_in_you = {
   order: 1,
-  src: 'https://www.youtube.com/embed/MFCzwiqlnCE',
+  src: 'https://www.youtube.com/embed/_9X2Tz7QegM',
   length: 3603,
   title: 'The Actor In You',
   description: `You don't need to be Mel Blanc or Matt Mercer to play many different characters at once! Practice your improv chops and learn how to craft compelling and differentiated NPCs for your party to love... or hate.`,
@@ -127,7 +127,7 @@ const actor_in_you = {
 
 const creatures = {
   order: 2,
-  src: 'https://www.youtube.com/embed/MFCzwiqlnCE',
+  src: 'https://www.youtube.com/embed/BVmbjUiR-YA',
   length: 3603,
   title: 'Lions and Tigers and Owlbears, Oh My!',
   description: `The D&D world is filled with fascinating creatures--they can be your players' enemies, allies, or even pets! Watch this video for some suggestions of creatures to incorporate into your story to scare and delight your players.`,
@@ -135,7 +135,7 @@ const creatures = {
 
 const treasure = {
   order: 3,
-  src: 'https://www.youtube.com/embed/MFCzwiqlnCE',
+  src: 'https://www.youtube.com/embed/8V-BrX3NRhs',
   length: 3603,
   title: 'Stuff Yourself',
   description: `Whether it's valuables hoarded by an evil red dragon you've just thwarted, a gift awarded by a grateful dryad, or simply the contents of a treasure chest in an abandoned dungeon room, interesting and exciting items are everywhere in the world of D&D... and this video shows how best to include them.`,
@@ -143,7 +143,7 @@ const treasure = {
 
 const random_tables = {
   order: 4,
-  src: 'https://www.youtube.com/embed/MFCzwiqlnCE',
+  src: 'https://www.youtube.com/embed/3hdGi_Bwjd8',
   length: 3603,
   title: 'Well, That Was Random.',
   description: `Random encounter tables are a DM's best friend. Learn how to create a list of possible situations and sights that fit your setting that will flavor your party's experience--and keep things always in the hands of fate!`,
@@ -167,7 +167,7 @@ const worldbuilding = {
 
 const rules_rolls = {
   order: 0,
-  src: 'https://www.youtube.com/embed/MFCzwiqlnCE',
+  src: 'https://www.youtube.com/embed/rKU2e5Xrx1k',
   length: 3603,
   title: 'Rules and Rolls',
   description: `Searching rooms, seducing spies, and triggering traps. This video covers the rules of rolling--learn how to respond when your players need to make a skill check, cast a saving throw, and more.`,
@@ -175,7 +175,7 @@ const rules_rolls = {
 
 const combat = {
   order: 1,
-  src: 'https://www.youtube.com/embed/MFCzwiqlnCE',
+  src: 'https://www.youtube.com/embed/cOGFmSXIvc8',
   length: 3603,
   title: 'Roll for Initiative!',
   description: `A combat encounter has begun! This can be one of the most daunting experiences for new D&D-ers, but with this video, you'll know just how to run any battle or brawl with the perfect balance of challenge and fun.`,
@@ -183,7 +183,7 @@ const combat = {
 
 const magic = {
   order: 2,
-  src: 'https://www.youtube.com/embed/MFCzwiqlnCE',
+  src: 'https://www.youtube.com/embed/YkwhS-RPEhw',
   length: 3603,
   title: 'Witchcraft and Wizardry',
   description: `Magic exists in the world of D&D, and it has its own strict set of rules. Learn the ins and outs of spellcasting, magic materials, and more.`,
@@ -191,7 +191,7 @@ const magic = {
 
 const unexpected = {
   order: 3,
-  src: 'https://www.youtube.com/embed/MFCzwiqlnCE',
+  src: 'https://www.youtube.com/embed/K0SjRJNENME',
   length: 3603,
   title: 'Expect the Unexpected',
   description: `So, your players were supposed to imprison the escaped orc convict... and they decided to adopt him as a foster son instead. In this video, learn what to do when everything goes exactly NOT as planned.`,
