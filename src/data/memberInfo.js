0863acd3f73b731b4e5f9eca9c75283e6f8238a5
Devon Marantz <devonmarantz@gmail.com>
2019-09-17 10:41:02 -0700

    Add memberInfo and pictures

---
+++ src/data/memberInfo.js
@@ -0,0 +1,37 @@
+/**
+ * Each team member and their related information
+ */
+const memberInfo = {
+  kevin: {
+    fullName: 'Kevin Baktiar',
+    src: `${process.env.PUBLIC_URL}/images/team/Kevin_Baktiar.jpg`,
+    role: 'Product Owner',
+    character: 'Art of Shadows',
+  },
+  sam: {
+    fullName: 'Sam Saks Fithian',
+    src: `${process.env.PUBLIC_URL}/images/team/Sam_Saks_Fithian.jpg`,
+    role: 'Software Engineer',
+    character: 'THE Dungeon Master',
+  },
+  emily: {
+    fullName: 'Emily Yang',
+    src: `${process.env.PUBLIC_URL}/images/team/Emily_Yang.png`,
+    role: 'Software Engineer',
+    character: 'Nyxa',
+  },
+  roberto: {
+    fullName: 'Roberto Sanchez',
+    src: `${process.env.PUBLIC_URL}/images/team/Roberto_Sanchez.jpg`,
+    role: 'Software Engineer',
+    character: 'The Birthday Boy',
+  },
+  devon: {
+    fullName: 'Devon Marantz',
+    src: `${process.env.PUBLIC_URL}/images/team/Devon_Marantz.jpg`,
+    role: 'Software Engineer',
+    character: 'Lord Honu',
+  },
+};
+
+export default memberInfo;
