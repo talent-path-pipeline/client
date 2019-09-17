dfc0b627bdc823b09ab5431ab5c3b52fb6e346d5
Sam Saks-Fithian <samsaksfithian@gmail.com>
2019-09-17 09:34:55 -0700

    lots of component refactoring, styling still in progress

--- src/components/catalog/CatalogCard.js
+++ src/components/catalog/CatalogCard.js
@@ -1,77 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import '../../css/catalog/CatalogCard.scss';
 
-export default class CatalogCard extends Component {
-  static propTypes = {
-    course: PropTypes.shape(PropTypes.object).isRequired,
-    clickCourse: PropTypes.func.isRequired,
-    slug: PropTypes.string.isRequired,
-  };
+const CatalogCard = ({ course }) => (
+  <div className="course-card">
+    <Link to={`/${course.slug}`}>
+      <img className="course-image" src={course.image_link} alt={course.title} />
+      <p className="course-title">{course.title}</p>
+      {/* TODO: calculate progress and display a progress bar component
+      <p className="course-progress">
+        {course.completedCourses} of {course.totalCourses} lessons completed
+      </p> */}
+    </Link>
+  </div>
+);
 
-  constructor(props) {
-    super(props);
+CatalogCard.propTypes = {
+  course: PropTypes.shape({
+    slug: PropTypes.string,
+    title: PropTypes.string,
+    description: PropTypes.string,
+    image_link: PropTypes.string,
+    order: PropTypes.number,
+    lessons: PropTypes.arrayOf(PropTypes.object),
+  }).isRequired,
+};
 
-    this.modalContainer = React.createRef();
-
-    this.state = {
-      course: {
-        title: '',
-        completedCourses: '',
-        totalCourses: '',
-        img: '',
-      },
-    };
-  }
-
-  // ===============================================================
-  // Handlers
-  componentDidMount() {
-    const { course } = this.state;
-    this.setState({
-      course: {
-        title: course.title,
-        completedCourses: course.completedCourses,
-        totalCourses: course.totalCourses,
-        img: course.img,
-      },
-    });
-  }
-
-  /**
-   * TODO: Handle route when course is clicked
-   * @param {Object} selectedCourse the object containing the data for the course to be displayed
-   */
-  clickCourse = selectedCourse => {
-    console.log(selectedCourse);
-  };
-
-  // ===============================================================
-  // Render
-
-  render() {
-    const { course, clickCourse, slug } = this.props;
-
-    return (
-      <div
-        id="course-card"
-        key={course.title}
-        role="button"
-        tabIndex={0}
-        onClick={() => clickCourse(course.title)}
-        onKeyPress={e => {
-          if (e.key === 'Enter') clickCourse(course.title);
-        }}
-      >
-        <Link to={`/${slug}`}>
-          <img className="course-image" src={course.img} alt="courseImage" />
-          <p className="course-title">{course.title}</p>
-          <p className="course-progress">
-            {course.completedCourses} of {course.totalCourses} lessons completed
-          </p>
-        </Link>
-      </div>
-    );
-  }
-}
+export default CatalogCard;
