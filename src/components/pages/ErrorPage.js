0e778403b560e08bf73aaf329d3a3c3baa29d9dc
emily-yang <cigoua@gmail.com>
2019-10-04 15:17:30 -0700

    style error page

--- src/components/pages/ErrorPage.js
+++ src/components/pages/ErrorPage.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../../css/pages/ErrorPage.scss';
 
-const ErrorPage = ({title, desc}) => (
-  <div>
+const ErrorPage = ({img, title, desc}) => (
+  <div className="error">
+    <img className="image" src={img} alt={title} />
     <h1>{title}</h1>
     <h3>{desc}</h3>
   </div>
 )
 
 ErrorPage.propTypes = {
+  img: PropTypes.string,
   title: PropTypes.string,
   desc: PropTypes.string,
 };
 
 ErrorPage.defaultProps = {
+  img: 'https://i.pinimg.com/474x/b2/a0/e3/b2a0e338f85f31a4a068276e662d8ff8.jpg',
   title: '404 Error',
   desc: 'Page not found',
 }
