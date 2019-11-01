0a15a6c06e3daa330d018e44c389a639b0eedf2a
Roberto Sanchez <rsanchez92@live.com>
2019-11-01 13:05:17 -0700

    Added real link for terms and agreement

--- src/components/registration/SignUp.js
+++ src/components/registration/SignUp.js
@@ -185,7 +185,7 @@ class SignUp extends Component {
               By clicking "Sign Up" you are agreeing
               to our
             {' '}
-            <a href="www.google.com">Terms and Agreement</a>
+            <a href="https://www.termsandcondiitionssample.com/live.php?token=bYAxBa2kby8ugr9x4eWMbKKgXnxOQyNg" target="_blank">Terms and Agreement</a>
           </p>
           <button
             id="register-button"
