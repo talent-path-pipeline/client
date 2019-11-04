82dad84c6fb16a7ffa776b563120b22432446ba6
emily-yang <cigoua@gmail.com>
2019-11-03 17:57:03 -0800

    add pageview tracking

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Router, Route, Redirect } from 'react-router-dom';
+import ReactGA from 'react-ga';
+import { createBrowserHistory } from 'history';
 import DUMMY_DATA from './DUMMY_DATA';
 import {
   NavBar,
@@ -27,55 +29,70 @@ const links = {
   dashboard: '/dashboard',
 };
 
-function App() {
-  const { courses } = DUMMY_DATA;
+const history = createBrowserHistory();
+history.listen(location => {
+  ReactGA.set({ page: location.pathname });
+  ReactGA.pageview(location.pathname);
+})
 
-  return (
-    <div id="start-page">
-      <NavBar links={links} />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route
-          exact
-          path={links.path}
-          render={props => <PathPage {...props} path_data={DUMMY_DATA} />}
-        />
-        <Route exact path={links.catalog} component={CatalogPage} />
-        {/* <Route exact path={links.login} component={RegistrationPage} /> */}
-        {/* <Route exact path={links.dashboard} component={DashboardPage} /> */}
-        <Route exact path={links.support} component={SupportPage} />
-        <Route exact path={links.about} component={AboutPage} />
 
-        <Redirect exact from="/courses/:course" to="/courses/:course/0" />
-        <Route
-          path="/courses/:course/:order"
-          render={props => {
-            const courseObj = courses.find(
-              course => course.slug === props.match.params.course,
-            );
-            const prevCourse = courses.find(course => course.order === courseObj.order - 1);
-            const nextCourse = courses.find(course => course.order === courseObj.order + 1);
-            const order = parseInt(props.match.params.order, 10);
-            if (!courseObj || order >= courseObj.lessons.length) return <ErrorPage />;
-            return (
-              <LessonPage
-                {...props}
-                course_title={courseObj.title}
-                lessons={courseObj.lessons}
-                curr_lesson_num={order}
-                base_path={courseObj.slug}
-                prev_slug={prevCourse ? prevCourse.slug : undefined}
-                next_slug={nextCourse ? nextCourse.slug : undefined}
+const { courses } = DUMMY_DATA;
+export default class App extends Component {
+  componentDidMount() {
+    ReactGA.pageview(window.location.pathname);
+  }
 
-              />
-            );
-          }}
-        />
-        <Route component={ErrorPage} />
-      </Switch>
-    </div>
-  );
+  render() {
+    return (
+      <Router history={history}>
+      <div id="start-page">
+        <NavBar links={links} />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route
+            exact
+            path={links.path}
+            render={props => <PathPage {...props} path_data={DUMMY_DATA} />}
+          />
+          <Route exact path={links.catalog} component={CatalogPage} />
+          {/* <Route exact path={links.login} component={RegistrationPage} /> */}
+          {/* <Route exact path={links.dashboard} component={DashboardPage} /> */}
+          <Route exact path={links.support} component={SupportPage} />
+          <Route exact path={links.about} component={AboutPage} />
+
+          <Redirect exact from="/courses/:course" to="/courses/:course/0" />
+          <Route
+            path="/courses/:course/:order"
+            render={props => {
+              const courseObj = courses.find(
+                course => course.slug === props.match.params.course,
+              );
+              const prevCourse = courses.find(course => course.order === courseObj.order - 1);
+              const nextCourse = courses.find(course => course.order === courseObj.order + 1);
+              const order = parseInt(props.match.params.order, 10);
+              if (!courseObj || order >= courseObj.lessons.length) return <ErrorPage />;
+              return (
+                <LessonPage
+                  {...props}
+                  course_title={courseObj.title}
+                  lessons={courseObj.lessons}
+                  curr_lesson_num={order}
+                  base_path={courseObj.slug}
+                  prev_slug={prevCourse ? prevCourse.slug : undefined}
+                  next_slug={nextCourse ? nextCourse.slug : undefined}
+
+                />
+              );
+            }}
+          />
+          <Route component={ErrorPage} />
+        </Switch>
+      </div>
+      </Router>
+    );
+  }
 }
+
 
 App.propTypes = {
   match: PropTypes.shape({
@@ -86,4 +103,3 @@ App.propTypes = {
   }).isRequired,
 };
 
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import App from './App';
 import ReactGA from 'react-ga';
 
 ReactGA.initialize(process.env.TRACKING_ID);
-ReactGA.pageview(window.location.pathname + window.location.search);
 
-ReactDOM.render(<Router><Route component={App} /></Router>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
