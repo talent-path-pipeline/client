cf3b4eab410b4f729d31692ec140044db39b7731
emily-yang <cigoua@gmail.com>
2019-11-03 17:19:41 -0800

    initialize ReactGA

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import App from './App';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize(process.env.TRACKING_ID);
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(<Router><Route component={App} /></Router>, document.getElementById('root'));
