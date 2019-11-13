add942aeda676e89d1b171caae8e771eb2e413a5
Devon Marantz <devonmarantz@gmail.com>
2019-11-13 11:43:15 -0800

    Add Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import stonehavenLogo from '/images/SANavbarLogo.png';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer className="pageFooter">
+        <p>
+          Product of <br />
+          <a href="https://talentpath.com/">Talent Path</a>
+        </p>
+        <div>
+          <div>
+            <img src={stonehavenLogo} alt="stonehavenLogo" />
+          </div>
+          <p>© 2019 Stonehaven Academy</p>
+        </div>
+        <a href="https://forms.gle/2YMiTeQ4iuZByx4ZA">Send Feedback!</a>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
