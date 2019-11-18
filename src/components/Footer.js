d11e462a201dddac46d4cbed09b027a02b0104f3
Sam Saks-Fithian <samsaksfithian@gmail.com>
2019-11-18 11:25:52 -0800

    made center footer section single <a> element, instead of two separate

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,12 +9,13 @@ const Footer = () => (
     </p>
     <div>
       <a href="https://github.com/talent-path-pipeline">
-        <img  className="footer-logo" src="/images/SA_IconLogo-Final.png" alt="stonehavenLogo" />
+        <img
+          className="footer-logo"
+          src="/images/SA_IconLogo-Final.png"
+          alt="stonehavenLogo"
+        />
+        <p>© 2019 Stonehaven Academy</p>
       </a>
-      <p>
-        © 2019 
-        <a href="https://github.com/talent-path-pipeline"> Stonehaven Academy</a>
-      </p>
     </div>
     <a href="https://forms.gle/2YMiTeQ4iuZByx4ZA">Send Feedback!</a>
   </footer>
