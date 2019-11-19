2ef3778dd0e322e8b2a30d46935a2f847576c801
Roberto Sanchez <rsanchez92@live.com>
2019-11-19 14:29:48 -0800

    Set up basic video tracking

--- src/components/pages/LessonPage.js
+++ src/components/pages/LessonPage.js
@@ -2,14 +2,62 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Lesson, LessonsPane } from '..';
 import '../../css/pages/LessonPage.scss';
+import TokenServices from '../../utils/tokenServices';
+import axios from 'axios';
+const { REACT_APP_SVR_API } = process.env;
 
 function LessonPage(props) {
   const { curr_lesson_num, lessons, base_path } = props;
   const lesson = lessons[curr_lesson_num];
   const total = lessons.length;
+const trackVideo = () =>{
+  const token = TokenServices.getToken();
+  const userID = token? token.id: null
+  
+  if(userID !== null){
+    const data = {videoID: lesson.video_id, userID}
+  //-------------------------------------------------------------
+  // For development purposes
+  console.log(`Video being watched: ${lesson.video_id} from ${props.course_title} by ${userID}`)
+  //-------------------------------------------------------------
+    /*
+    axios
+    .post(`${REACT_APP_SVR_API}/lesson/`, data)
+    .then(response => {
+      console.log(`This is the response: ${response}`)
+    })
+    .catch(error => {
+      try {
+        // Handles errors that are not HTTP specific
+        console.error(error);
+        if (!error.status) {
+          console.error('A network error has occured.');
+        } else if (error.response.status === 400) {
+          console.error('Bad Request');
+        } else if (error.response.status === 500) {
+          console.error('Something bad happended on the server.');
+        } else {
+          console.error('An unknown error has occurred');
+        }
+      } catch (ex) {
+        alert('Something went wrong...');
+        Promise.reject(ex);
+      }
+    });   
+     */
+    
+  }else{
+    //-------------------------------------------------------------
+  // For development purposes
+  console.log(`Video being watched: ${lesson.video_id} by unregistered user`)
+  //-------------------------------------------------------------
+  }
+  
 
+}
   return (
     <div className="lesson-page">
+      {trackVideo()}
       <Lesson lesson={lesson} course_size={total} base_path={base_path} />
       <LessonsPane {...props} />
     </div>
