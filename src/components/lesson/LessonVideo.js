44265938cbaa339519c52d90c63e73080a396e72
emily-yang <cigoua@gmail.com>
2019-11-20 11:49:15 -0800

    convert youtube iframe to react-youtube component

--- src/components/lesson/LessonVideo.js
+++ src/components/lesson/LessonVideo.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import YouTube from 'react-youtube';
 import '../../css/lesson/LessonVideo.scss';
 
 const LessonVideo = ({ title, video_id, start, end }) => {
@@ -9,18 +10,35 @@ const LessonVideo = ({ title, video_id, start, end }) => {
   const both = valid_start && valid_end ? '&' : '';
   const startStr = valid_start ? `start=${start}` : '';
   const endStr = valid_end ? `end=${end}` : '';
-  const play_segment = `${either}${startStr}${both}${endStr}`;
+  // const play_segment = `${either}${startStr}${both}${endStr}`;
+
+  const opts = {
+    title,
+    width: '560',
+    height: '325',
+    playerVars: {
+      allowFullScreen: 'allowfullscreen',
+      frameBorder: '0',
+      start,
+      end,
+    },
+  };
 
   return (
     // TODO: maybe specify specific size for iframe player so it's not bigger/smaller for different videos?
     <div className="lesson-video">
-      <iframe
+      {/* <iframe
         title={title}
         width="560"
         height="325"
         allowFullScreen="allowfullscreen"
         frameBorder="0"
         src={`https://www.youtube.com/embed/${video_id}${play_segment}`}
+      /> */}
+
+      <YouTube
+        videoId={video_id}
+        opts={opts}
       />
     </div>
   );
