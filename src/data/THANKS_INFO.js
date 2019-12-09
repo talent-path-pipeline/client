e5275475acb0d9efabb43f178268173dc81044dc
Sam Saks-Fithian <samsaksfithian@gmail.com>
2019-12-09 11:05:40 -0800

    content updates, minor styling tweaks

--- src/data/THANKS_INFO.js
+++ src/data/THANKS_INFO.js
@@ -12,8 +12,67 @@ const thanks = [
     details:
       'Thank you for helping plan out the structure for our courses and lessons, and writing the very good descriptions!',
   },
-  // D&D Community / all the great content creators
-  // DnDBeyond?
+  {
+    img_src:
+      'https://i.pinimg.com/originals/de/1c/91/de1c91788be0d791135736995109272a.png',
+    name: 'D&D Content on YouTube',
+    roles: ['Awesome Content Creators', 'Welcoming Community'],
+    nickname: '',
+    nickname_link: '',
+    contact_link:
+      'https://www.youtube.com/results?search_query=how+to+play+dungeons+and+dragons',
+    details: `Thank you to all of the awesome content creators on YouTube who help make the game 
+      super open and accessible! Shout outs to: WASD20, The DM Lair, Nerdarchy, How to Be 
+      a Great GM, Geek & Sundry, Don't Stop Thinking, Davvy Chappy, Puffin Forest, and so many more!`,
+  },
+  {
+    img_src:
+      'https://upload.wikimedia.org/wikipedia/en/thumb/9/90/Wizards_of_the_Coast_logo.svg/1200px-Wizards_of_the_Coast_logo.svg.png',
+    name: 'Wizards of the Coast',
+    roles: ['Wonderful Nerdy Publishers', 'Amazing Worldbuilders and Rule Designers'],
+    nickname: '',
+    nickname_link: '',
+    contact_link: 'https://dnd.wizards.com/',
+    details:
+      'Thank you for publishing and maintaining such an awesome game that helps so many people get together and express themselves and their nerdiness! We love and appreciate all of you and your hard work! (Especially Chris, Kate, and Jeremy <3 -Sam)',
+  },
+  {
+    img_src: 'https://pbs.twimg.com/profile_images/1082330444845277187/eRmykg4g.jpg',
+    name: 'DnDBeyond',
+    roles: ['All-Around Useful Tool'],
+    nickname: '',
+    nickname_link: '',
+    contact_link: 'http://www.dndbeyond.com',
+    details:
+      'Thank you for being just an awesome site that has great info, character sheets, and web design! You helped Sam get the rest of the team into D&D!',
+  },
+  {
+    img_src:
+      'https://icv2.com/images/article_thumbs/650x650_84e08cb84a790c33b550546e291cedf4a6e52eabbccf555cf4da59dc.jpg',
+    name: 'The Adventure Zone',
+    roles: [
+      'Actual-Play Podcast',
+      'Hilarious Goofs',
+      'Genuinely Moving Story',
+      'Questionable Game Rule Knowledge',
+    ],
+    nickname: '',
+    nickname_link: '',
+    contact_link: 'https://maximumfun.org/podcasts/adventure-zone/',
+    details:
+      'Thank you for being so funny, entertaining, moving, and just all-around awesome. TAZ: Balance in particular has, and always will have, a special place in our hearts <3',
+  },
+  {
+    img_src:
+      'https://ksr-ugc.imgix.net/assets/021/819/868/99f47e171a602c8c46eaaae6b677e1b2_original.png?ixlib=rb-2.1.0&w=700&fit=max&v=1530910872&auto=format&frame=1&q=92&s=c095313a391ae0bba6816dae8f9327dd',
+    name: 'Critical Role',
+    roles: ['Actual-Play Stream', 'Great Voices', 'Actual Game Rule Knowledge'],
+    nickname: '',
+    nickname_link: '',
+    contact_link: 'https://critrole.com/',
+    details:
+      'Thank you for making such great content, and helping popularize and bring D&D to the masses!',
+  },
 ];
 
 export default thanks;
